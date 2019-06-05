5c6f687e403c70120b73a42b8387285533845aac
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2019-06-05 03:12:56 -0500

    Finishing UI Reset Password and conections

--- app/src/main/java/com/example/firebase/ResetPasswordActivity.java
+++ app/src/main/java/com/example/firebase/ResetPasswordActivity.java
@@ -2,6 +2,7 @@ package com.example.firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class ResetPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+    }
+
+    public void goBack(View view){
+        finish();
     }
 }
