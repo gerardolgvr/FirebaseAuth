2d85f2502131fb24c600f7e99f01b2d7501d1f8f
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2019-06-10 21:56:57 -0500

    Updating email

--- app/src/main/java/com/example/firebase/DialogEmail.java
+++ app/src/main/java/com/example/firebase/DialogEmail.java
@@ -9,19 +9,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 
-public class Dialog extends AppCompatDialogFragment {
+public class DialogEmail extends AppCompatDialogFragment {
     private EditText editText;
-    private DialogListener listener;
+    private DialogEmailListener listener;
 
     @Override
     public android.app.Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.text_input, null);
+        View view = inflater.inflate(R.layout.email_input, null);
 
         builder.setView(view)
-                .setTitle("Your password:")
+                .setTitle("Your Email:")
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -31,12 +31,12 @@ public class Dialog extends AppCompatDialogFragment {
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        String passsword = editText.getText().toString();
-                        listener.getPassword(passsword);
+                        String email = editText.getText().toString();
+                        listener.getEmail(email);
                     }
                 });
 
-        editText = (EditText) view.findViewById(R.id.input);
+        editText = (EditText) view.findViewById(R.id.edit_email_input);
 
         return  builder.create();
     }
@@ -45,14 +45,14 @@ public class Dialog extends AppCompatDialogFragment {
     public void onAttach(Context context) {
         super.onAttach(context);
         try {
-            listener = (DialogListener) context;
+            listener = (DialogEmailListener) context;
         } catch (ClassCastException e){
-            throw  new ClassCastException(context.toString() + "must implement DialogListener");
+            throw  new ClassCastException(context.toString() + "must implement DialogEmailListener");
         }
 
     }
 
-    public interface DialogListener {
-        void getPassword(String password);
+    public interface DialogEmailListener {
+        void getEmail(String email);
     }
 }
--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -1,22 +1,13 @@
 package com.example.firebase;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.text.InputType;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -24,14 +15,15 @@ import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity implements Dialog.DialogListener {
+public class MainActivity extends AppCompatActivity implements Dialog.DialogListener, DialogEmail.DialogEmailListener {
     //firebase
     private FirebaseAuth mAuth;
     private FirebaseUser user;
 
     private String password;
+    private String operation;
 
-    Button btn_signOut, btn_remove_user;
+    Button btn_signOut, btn_remove_user, btn_change_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +40,9 @@ public class MainActivity extends AppCompatActivity implements Dialog.DialogList
         //ui
         btn_signOut = (Button) findViewById(R.id.btn_sing_out);
         btn_remove_user = (Button) findViewById(R.id.btn_remove_user);
+        btn_change_email = (Button) findViewById(R.id.btn_change_email);
 
-        //event
+        //event sign out
         btn_signOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,10 +50,21 @@ public class MainActivity extends AppCompatActivity implements Dialog.DialogList
             }
         });
 
-        //event
+        //event remove account
         btn_remove_user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                operation = "remove";
+                openDialog();
+            }
+        });
+
+        //event change email
+        btn_change_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operation = "updateEmail";
+                openEmailDialog();
                 openDialog();
             }
         });
@@ -86,35 +90,42 @@ public class MainActivity extends AppCompatActivity implements Dialog.DialogList
         dialog.show(getSupportFragmentManager(), "Dialog");
     }
 
+    public void openEmailDialog(){
+        DialogEmail dialog = new DialogEmail();
+        dialog.show(getSupportFragmentManager(), "DialogEmail");
+    }
+
 
     @Override
     public void getPassword(String password) {
-        this.password = password;
-
         //validating input data
         if(password.equals("")){
             Toast.makeText(getApplicationContext(), "Error! Password required", Toast.LENGTH_SHORT).show();
             return;
         }
-        AuthCredential credential = EmailAuthProvider
-                .getCredential(user.getEmail(), password);
-        user.reauthenticate(credential)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        user.delete()
-                                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<Void> task) {
-                                        if (task.isSuccessful()) {
-                                            Toast.makeText(getApplicationContext(), "Account Deleted!", Toast.LENGTH_SHORT).show();
-                                            goLoginActivity();
+        if(operation == "remove"){
+            AuthCredential credential = EmailAuthProvider
+                    .getCredential(user.getEmail(), password);
+            user.reauthenticate(credential)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            user.delete()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(getApplicationContext(), "Account Deleted!", Toast.LENGTH_SHORT).show();
+                                                goLoginActivity();
+                                            }
                                         }
-                                    }
 
-                                });
-                    }
-                });
+                                    });
+                        }
+                    });
+        } else if(operation == "updateEmail"){
+            this.password = password;
+        }
 
     }
 
@@ -122,5 +133,30 @@ public class MainActivity extends AppCompatActivity implements Dialog.DialogList
     public void goLoginActivity(){
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void getEmail(final String email) {
+        //validating input data
+        if(email.equals("")){
+            Toast.makeText(getApplicationContext(), "Error! new email required", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        AuthCredential credential = EmailAuthProvider
+                .getCredential(user.getEmail(), this.password);
+        user.reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        user.updateEmail(email)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        Toast.makeText(getApplicationContext(), "Email Updated!", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }
+                });
     }
 }
